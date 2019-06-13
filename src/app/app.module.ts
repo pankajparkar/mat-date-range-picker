df73984882ea00eb70b89ebcbc8a1709e671bc24
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-06-14 02:51:09 +0530

    Added material and datepicker onto the page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatDatepickerModule, MatFormFieldModule } from '@angular/material';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import { CustomMaterialModule } from './custom-material.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Material module
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
