aaa1f21af974b03f3bb2f75cf688dd4cf2801cbe
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-06-14 02:04:56 +0530

    Added angular CLI generated commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'drp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mat-date-range-picker';
+}
