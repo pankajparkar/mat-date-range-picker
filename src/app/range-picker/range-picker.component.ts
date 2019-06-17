565f916d122c80503a3c1a4177f4ed5c7332854b
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-06-17 23:53:58 +0530

    Minimal highlighting feature of selected range.

--- src/app/range-picker/range-picker.component.ts
+++ src/app/range-picker/range-picker.component.ts
@@ -14,11 +14,13 @@ export class RangePickerComponent implements OnInit {
   @ViewChild('endDate', {read: MatCalendar}) endDate;
   dateRange: FormGroup;
   startAt = new Date();
-  endAt = this.dateAdapter.addCalendarMonths(this.startAt, 1)
+  endAt;
 
   constructor (private fb: FormBuilder, private dateAdapter: DateAdapter<Date>) {}
 
   ngOnInit () {
+    const endAt = this.dateAdapter.addCalendarMonths(this.startAt, 1)
+    this.endAt = new Date(endAt.getFullYear(), endAt.getMonth(), 1)
     this.dateRange = this.fb.group({
       startDate: new FormControl(),
       endDate: new FormControl()
@@ -27,16 +29,28 @@ export class RangePickerComponent implements OnInit {
 
   selectStartDate(date) {
     const startDate = this.dateRange.controls['startDate']
-    startDate.setValue(date)
+    startDate.setValue(date);
   }
 
   selectEndDate(date) {
     const endDate = this.dateRange.controls['endDate']
-    endDate.setValue(date)
+    endDate.setValue(date);
   }
 
   close () {
-    console.log('close')
+    // Fire selection vague DOM manipulation
+    const endDate = this.dateRange.controls['endDate'].value.getDate(),
+      startDate = this.dateRange.controls['startDate'].value.getDate();
+    console.log(this.startDate, this.endDate)
+    const [startMonth, endMonth] = Array.from(document.querySelectorAll('.mat-calendar-body'));
+    const startMonthCells = Array.from(startMonth.querySelectorAll('.mat-calendar-body-cell'));
+    const endMonthCells = Array.from(endMonth.querySelectorAll('.mat-calendar-body-cell'));
+    startMonthCells.forEach((cell: HTMLElement) => {
+      if (startDate <= cell.textContent) cell.style.background = 'red'
+    })
+    endMonthCells.forEach((cell: HTMLElement) => {
+      if (endDate >= cell.textContent) cell.style.background = 'red'
+    })
   }
 
 }
