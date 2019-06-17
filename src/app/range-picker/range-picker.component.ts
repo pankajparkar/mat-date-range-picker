c0deae14188bb609283329d4c047d6709883a7c5
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-06-17 22:29:30 +0530

    1. Removed datepicker 2. Use DateAdapter.

--- src/app/range-picker/range-picker.component.ts
+++ src/app/range-picker/range-picker.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { map, merge, combineLatest, debounceTime, filter } from 'rxjs/operators';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
-import { DateService } from '../date.service';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'drp-range-picker',
@@ -16,48 +14,26 @@ export class RangePickerComponent implements OnInit {
   @ViewChild('endDate', {read: MatDatepicker}) endDate;
   dateRange: FormGroup;
   startAt = new Date();
-  endAt = this.dateService.addMonths(new Date(), 1)
+  endAt = this.dateAdapter.addCalendarMonths(this.startAt, 1)
 
-  constructor (private fb: FormBuilder, private dateService: DateService) {}
+  constructor (private fb: FormBuilder, private dateAdapter: DateAdapter<Date>) {}
   ngAfterViewInit () {
     console.log('ngAfterViewInit', this.startDate)
   }
 
   ngOnInit () {
-    debugger
     this.dateRange = this.fb.group({
       startDate: new FormControl(),
       endDate: new FormControl()
     })
   }
 
-  bindEvents () {
-    // TODO: change it to use Renderer2
-    const matCalendarTable = document.querySelectorAll(".mat-calendar-table tbody")
-    console.log(matCalendarTable)
-    
-    const enter = fromEvent(matCalendarTable, "mouseenter")
-    const leave = fromEvent(matCalendarTable, "mouseleave")
-    const move  = fromEvent(matCalendarTable, "mousemove")
-    const entered = enter.pipe(
-      map(_ => true),
-      merge(
-        leave.pipe(
-          map(_ => false)
-        )
-      )
-    )
-    
-    move
-      .pipe(
-        combineLatest(entered),
-        debounceTime(500),
-        filter(([e, b]) => {
-          console.log(e, b)
-          return b
-        }),
-        map(([e, _]) => console.log(e, _))
-      )
+  select(event) {
+    console.log('select', event)
+  }
+
+  close () {
+    console.log('close')
   }
 
 }
